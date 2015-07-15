405d38d3e524a097c86c3ae9d350baad2c69cf9b
Dontloo <wdl.stan@gmail.com>
2015-07-15 12:13:43 +1000

    Update align_data.m

--- align_data.m
+++ align_data.m
@@ -1,3 +1,4 @@
+% transfer single channel (grayscale) data into the form of m*n*1*num
 function [x] = align_data(x)
     if numel(size(x))<4
         x = reshape(x,[size(x,1) size(x,2) 1 size(x,3)]);
